f38d46510aeef541fa358c7efeb475a4c4d5ccb5
charlemagne <lemuelpuglisi001@gmail.com>
2019-09-12 12:25:28 +0200

    Add neaarest tree searching

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -1,7 +1,7 @@
-import { coordinates } from "../utils";
+import { dist, coordinates } from "../utils";
 
 export default class Enemy {
-    constructor(ctx, x, y, depth) {
+    constructor(ctx, x, y, depth, trees) {
         this.sprite = ctx.add.sprite(x, y)
         this.sprite.depth = depth
 
@@ -12,6 +12,8 @@ export default class Enemy {
 
         this.active = false
 
+        this.trees = trees; 
+        this.target = null; 
         this.hp = 100
     }
 
@@ -22,12 +24,15 @@ export default class Enemy {
     update(ctx, spawnDifficulty) {
         if(this.active) {
             if(this.hp <= 0) {
-                this.sprite.y = this.maxY
+                this.respawn(); 
             }
 
             if(this.sprite.y < this.maxY) {
-                this.sprite.y += this.speed
-                // this.sprite.depth = this.sprite.y
+                // look for the nearest tree
+                if (this.target == null) {
+                    this.target = this.getNearestTree(); 
+                }
+                this.walkToTarget(); 
             } else {
                 this.active = false
             }
@@ -38,5 +43,46 @@ export default class Enemy {
                 this.sprite.y = this.minY
             }
         }
+    }
+
+    getNearestTree() {
+        let target = null; 
+        let min = null;
+        for (let i = 0; i < this.trees.length; i++) {
+            let x = this.trees[i].sprite.x;
+            let y = this.trees[i].sprite.y;
+            console.log(this.trees[i].sprite)
+            console.log('tree x ' + x)
+            console.log('tree y ' + y)
+            console.log('enemy x ' + this.sprite.x)
+            console.log('enemy y ' + this.sprite.y)
+            let distance = dist(this.sprite.x, this.sprite.y, x, y);
+            console.log(distance);
+            min = min == null ? distance : min; 
+            if (distance < min) {
+                target = this.trees[i]; 
+                min = distance; 
+            }
+        }
+        return target; 
+    }
+
+    walkToTarget() {
+        if (!this.isNear(this.target)) {
+            this.sprite.x = this.sprite.x > this.target.x ? this.sprite.x -- : this.sprite.x ++; 
+            this.sprite.y = this.sprite.y > this.target.y ? this.sprite.y -- : this.sprite.y ++; 
+            return true; 
+        }
+        return false; 
+    }
+
+    isNear(tree) {
+        let response = Math.abs(this.sprite.x - tree.x) > 3;
+        return response &= Math.abs(this.sprite.y - tree.y) > 3;
+    }
+
+    respawn() {
+        this.sprite.y = this.maxY
+        this.hp = 100; 
     }
 }
