cd6932a69d2a2bac448f71e4a2ae0f3f0cebd443
weelix <smaugeri97@gmail.com>
2019-09-12 16:16:00 +0200

    Improved attack way of enemies

--- src/sprites/Tree.js
+++ src/sprites/Tree.js
@@ -2,6 +2,7 @@ export default class Tree {
     constructor(ctx, x, y, depth) {
         this.sprite = ctx.add.sprite(x, y)
         this.sprite.depth = depth
+        this.hp = 100
     }
 
     playAnim(animId) {
